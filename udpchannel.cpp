955fda3abc04cc2061268a52075222962c879dcc
zy <zybl@ZYBEELINK>
2024-03-18 22:01:41 -0400

    udpchannel add multicast support

--- udpchannel.cpp
+++ udpchannel.cpp
@@ -9,47 +9,45 @@ using ip::udp;
 using namespace std; // for std::bind and std::placeholders
 
 // UDP Channel class
-class UDPChannel
-{
+class UDPChannel {
 public:
-    UDPChannel(io_service &ioService, uint16_t port)
-        : socket_(ioService, udp::endpoint(udp::v4(), port)) {}
+    UDPChannel(io_service& ioService, uint16_t port, const ip::address& multicastAddress)
+        : socket_(ioService, udp::endpoint(udp::v4(), port)) {
+        multicastEndpoint_ = udp::endpoint(multicastAddress, port);
+        joinMulticastGroup();
+    }
 
-    void startReceive(function<void(const string &)> callback)
-    {
+    void startReceive(function<void(const string&)> callback) {
         callback_ = callback;
         receiveNext();
     }
 
-    void send(const string &message, const udp::endpoint &remoteEndpoint)
-    {
+    void send(const string& message, const udp::endpoint& remoteEndpoint) {
         socket_.async_send_to(
             buffer(message + "\n", message.length() + 1), remoteEndpoint,
-            [](const boost::system::error_code &error, std::size_t bytesTransferred)
-            {
-                if (error)
-                {
+            [](const boost::system::error_code& error, std::size_t bytesTransferred) {
+                if (error) {
                     std::cerr << "Send error: " << error.message() << std::endl;
                 }
             });
     }
 
-    void send(const char *message, size_t length, const udp::endpoint &remoteEndpoint)
-    {
+    void send(const char* message, size_t length, const udp::endpoint& remoteEndpoint) {
         socket_.async_send_to(
             buffer(message, length), remoteEndpoint,
-            [](const boost::system::error_code &error, std::size_t bytesTransferred)
-            {
-                if (error)
-                {
+            [](const boost::system::error_code& error, std::size_t bytesTransferred) {
+                if (error) {
                     std::cerr << "Send error: " << error.message() << std::endl;
                 }
             });
     }
 
 private:
-    void receiveNext()
-    {
+    void joinMulticastGroup() {
+        socket_.set_option(ip::multicast::join_group(multicastEndpoint_.address()));
+    }
+
+    void receiveNext() {
         socket_.async_receive_from(
             buffer(temp_buffer_), remoteEndpoint_,
             std::bind(&UDPChannel::handleReceive, this, std::placeholders::_1, std::placeholders::_2));
@@ -62,72 +60,65 @@ private:
             string message(temp_buffer_.data(), bytesTransferred);
             callback_(message);
             receiveNext(); // Start receiving the next message
-        }
-        else
-        {
+        } else {
             std::cerr << "Receive error: " << error.message() << std::endl;
         }
     }
 
     udp::socket socket_;
+    udp::endpoint multicastEndpoint_;
     udp::endpoint remoteEndpoint_;
     array<char, 4096> temp_buffer_;
     function<void(const string &)> callback_;
 };
 
 // UDP Publisher
-class UDPPublisher
-{
+class UDPPublisher {
 public:
-    UDPPublisher(io_service &ioService, uint16_t localPort, const udp::endpoint &remoteEndpoint)
-        : channel_(ioService, localPort)
-    {
-        remoteEndpoint_ = remoteEndpoint;
+    UDPPublisher(io_service& ioService, uint16_t localPort, const ip::address& multicastAddress, uint16_t multicastPort)
+        : channel_(ioService, localPort, multicastAddress) {
+        multicastEndpoint_ = udp::endpoint(multicastAddress, multicastPort);
     }
 
-    void publish(const string &message)
-    {
-        channel_.send(message, remoteEndpoint_);
+    void publish(const string& message) {
+        channel_.send(message, multicastEndpoint_);
     }
 
-    void publish(const char *message, size_t length)
-    {
-        channel_.send(message, length, remoteEndpoint_);
+    void publish(const char* message, size_t length) {
+        channel_.send(message, length, multicastEndpoint_);
     }
 
 private:
     UDPChannel channel_;
-    udp::endpoint remoteEndpoint_;
+    udp::endpoint multicastEndpoint_;
 };
 
 // UDP Receiver
-class UDPReceiver
-{
+class UDPReceiver {
 public:
-    UDPReceiver(io_service &ioService, uint16_t port)
-        : channel_(std::make_unique<UDPChannel>(ioService, port)) {}
+    UDPReceiver(io_service& ioService, uint16_t port, const ip::address& multicastAddress)
+        : channel_(std::make_unique<UDPChannel>(ioService, port, multicastAddress)) {}
 
-    void startReceiving()
-    {
+    void startReceiving() {
         channel_->startReceive(handleMessage);
     }
 
 private:
-    static void handleMessage(const string &message)
-    {
+    static void handleMessage(const string& message) {
         std::cout << "Received message: " << message << std::endl;
     }
 
     std::unique_ptr<UDPChannel> channel_;
 };
 
-int main()
-{
+int main() {
     io_service ioService;
 
-    // Create a publisher and a receiver
-    UDPPublisher publisher(ioService, 0, udp::endpoint(ip::address::from_string("127.0.0.1"), 8000));
-    UDPReceiver receiver(ioService, 8000);
+    // Create a publisher and a receiver for multicast
+    ip::address multicastAddress = ip::address::from_string("239.255.0.1");
+    uint16_t multicastPort = 8000;
+    UDPPublisher publisher(ioService, 0, multicastAddress, multicastPort);
+    UDPReceiver receiver(ioService, multicastPort, multicastAddress);
 
     // Start receiving messages
     receiver.startReceiving();
